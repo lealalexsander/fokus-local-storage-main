b4726f7d8c78272279b27020d21af22e40585567
Alexsander Leal <alexsanderunr@gmail.com>
2024-11-26 15:09:09 -0300

    adicionado a API localStorage, para armazenamento local dos dados

--- script-crud.js
+++ script-crud.js
@@ -1,8 +1,21 @@
 const btnAdicionarTarefa = document.querySelector('.app__button--add-task');
 const formAdicionarTarefa = document.querySelector('.app__form-add-task');
+const textArea = documento.querySelector('.app__form-textarea');
+
+
+const tarefas = [];
 
 
 btnAdicionarTarefa.addEventListener('click', () => {
     formAdicionarTarefa.classList.toggle('hidden');
 
+})
+
+formAdicionarTarefa.addEventListener('submit', (evento) => {
+    evento.preventDefault(); //previne um evento padrão
+    const tarefa = {
+        descricao: textArea.value
+    }
+    tarefas.push(tarefa);
+    localStorage.setItem('tarefas', JSON.stringify(tarefas));
 })
